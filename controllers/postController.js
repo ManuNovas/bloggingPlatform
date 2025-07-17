f70d4a5023f22b10d036cf95b88a05af4fd0e5a9
ManuNovas <enam_rda@yahoo.com.mx>
2025-07-17 11:55:03 -0600

    Add `get` and `getAll` endpoints and their handlers for posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -52,6 +52,43 @@ const postController = {
       response.status(500).send('Ocurrió un error al eliminar la publicación');
     });
   },
+  get: function (request, response) {
+    const {id} = request.params;
+    Post.findById(id).then(post => {
+      if (post) {
+        response.status(200).json(post);
+      } else {
+        postController.notFoundResponse(response);
+      }
+    }).catch(error => {
+      console.log(error);
+      response.status(500).send('Ocurrio un error al obtener la publicacion');
+    });
+  },
+  getAll: function (request, response) {
+    const {term} = request.query;
+    let params = {};
+    if (term) {
+      params = {
+        $or: [
+          {title: {$regex: term, $options: 'i'}},
+          {content: {$regex: term, $options: 'i'}},
+          {category: {$regex: term, $options: 'i'}},
+          {tags: {$regex: term, $options: 'i'}}
+        ],
+      };
+    }
+    Post.find(params).then(posts => {
+      if (posts) {
+        response.status(200).json(posts);
+      } else {
+        postController.notFoundResponse(response);
+      }
+    }).catch(error => {
+      console.log(error);
+      response.status(500).send('Ocurrio un error al obtener las publicaciones');
+    });
+  },
 };
 
 module.exports = postController;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 router.post('/', postController.create);
 router.put('/:id', postController.update);
 router.delete('/:id', postController.delete);
+router.get('/:id', postController.get);
+router.get('/', postController.getAll);
 
 module.exports = router;
